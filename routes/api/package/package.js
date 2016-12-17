f14166bef5e2d8dcd3bb30b4d3d83e7ceef584ff
Liam Jones <Liam@Acelinecomputing.com>
2016-12-17 22:54:51 +0000

    Updated Package api API now checks if supplied email is in the db. If so returns "trial", otherwise, returns "none"

--- routes/api/package/package.js
+++ routes/api/package/package.js
@@ -2,12 +2,24 @@ var keystone = require('keystone');
 
 exports = module.exports = function (req, res) {
 
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
-	console.log("wregfw")
+	//var view = new keystone.View(req, res);
+	//var locals = res.locals;
+	//console.log("wregfw")
 	var email = req.query.email;
 	if(email){
-		return res.apiResponse({package:"trial"});
+		var decodedEmail = decodeURI(email);
+		var UserDb = keystone.list("User").model;
+		UserDb.findOne()
+			.where("email", email)
+			.exec(function(err, user){
+				if(!err && user)
+				{
+					return res.apiResponse({package:"trial"});
+				}
+				else{
+					return res.apiResponse({package:"none"});
+				}
+			})
+		
 	}
-	return res.apiResponse({success:false});
 };
